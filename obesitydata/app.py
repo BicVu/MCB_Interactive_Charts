c353af48ff4d10f54c6cea1e0695f118e501b9df
Bic <BicVu@users.noreply.github.com>
2019-07-20 19:40:56 -0400

    edit app.py for sqlite

--- obesitydata/app.py
+++ obesitydata/app.py
@@ -3,6 +3,22 @@ from flask import render_template
 
 app = Flask(__name__)
 
+#################################################
+# Database Setup
+#################################################
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+Samples_Metadata = Base.classes.sample_metadata
+Samples = Base.classes.samples
+
 @app.route("/home")
 def main():
     return render_template('index.html')
